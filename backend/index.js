39a0a36ac6e84dffceb001c60b396a4b1a9b1464
weber <santhoshpandi543@gmail.com>
2025-02-27 21:44:37 +0530

    cors origin all

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONT_END_URL,
+  origin: "*",
   credentials: true,
 }))
 
