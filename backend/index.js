0ee3d279b79cffc018112f8e9a62b505931f9ed8
weber <santhoshpandi543@gmail.com>
2025-02-28 17:05:46 +0530

    cors disabled

--- backend/index.js
+++ backend/index.js
@@ -18,20 +18,20 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
-app.use(cors({
-  origin: process.env.FRONT_END_URL,
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: process.env.FRONT_END_URL,
+//   credentials: true,
+// }))
 
 
-// app.use((req, res, next) => {
-//   console.log(`CORS middleware triggered for ${req.method} ${req.url}`);
-//   res.header('Access-Control-Allow-Origin', 'https://food-hub-cyan.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log(`CORS middleware triggered for ${req.method} ${req.url}`);
+  res.header('Access-Control-Allow-Origin', 'https://food-hub-cyan.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 
 
