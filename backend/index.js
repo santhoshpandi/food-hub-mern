f3f39bf39326d5893ba99f49a86b16253fdfed61
weber <santhoshpandi543@gmail.com>
2025-02-28 16:07:02 +0530

    allow header instead of cors

--- backend/index.js
+++ backend/index.js
@@ -19,17 +19,29 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
-app.use(cors({
-  origin: process.env.FRONT_END_URL,
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: process.env.FRONT_END_URL,
+//   credentials: true,
+// }))
+
+
+app.use((req, res, next) => {
+  res.header(
+  'Access-Control-Allow-Origin',
+  process.env.FRONT_END_URL
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  
+  console.log('Request received:', req.method, req.url);
+  
+  next();
+  });
+
 
 console.log(process.env.FRONT_END_URL)
 foodDB()
-app.use((req,res,next)=>{
-  console.log(req.method+" "+req.url)
-  next()
-})
 
 //-----------------------------------------------------
 
