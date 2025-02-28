6faffe5c557560edd3492af842009bbda64351f6
weber <santhoshpandi543@gmail.com>
2025-02-28 16:18:17 +0530

    particular name given

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 app.use((req, res, next) => {
   res.header(
   'Access-Control-Allow-Origin',
-  process.env.FRONT_END_URL
+  'https://food-hub-cyan.vercel.app'
   );
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
